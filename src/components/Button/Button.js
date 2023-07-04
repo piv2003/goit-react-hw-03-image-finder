f0d2d38191435aacf5d5c31781aca6ebf5d651d5
Ihor Poliakov <piv2003@gmail.com>
2023-07-04 23:10:35 +0200

    Update Button.js

--- src/components/Button/Button.js
+++ src/components/Button/Button.js
@@ -1,4 +1,8 @@
 import { Button } from './ButtonLoadMore.styled';
 import PropTypes from 'prop-types';
 
+ButtonLoadMore.propTypes = {
+  onBtnLoadMore: PropTypes.func.isRequired,
+};
+
 export default ButtonLoadMore;
