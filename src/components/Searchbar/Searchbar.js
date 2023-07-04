49c8efda83e2591a6d7f80c4e9704f3c8b4a1ef7
Ihor Poliakov <piv2003@gmail.com>
2023-07-04 22:40:28 +0200

    add SearchBar

---
+++ src/components/Searchbar/Searchbar.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import { GoSearch } from 'react-icons/go';
+import { toast } from 'react-toastify';
+import {
+  SearchbarBox,
+  SearchbarInput,
+  SearchButton,
+  SearchForm,
+} from './Searchbar.styled';
