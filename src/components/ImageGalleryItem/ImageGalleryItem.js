11a56593ae83388cbc7b41c397e831d386f54066
Ihor Poliakov <piv2003@gmail.com>
2023-07-04 23:27:03 +0200

    Update ImageGalleryItem.js

--- src/components/ImageGalleryItem/ImageGalleryItem.js
+++ src/components/ImageGalleryItem/ImageGalleryItem.js
@@ -1,4 +1,9 @@
 import { ImageItem } from './ImageGalleryItem.styled';
 import PropTypes from 'prop-types';
 
+ImageGalleryItem.propTypes = {
+  smallImageURL: PropTypes.string.isRequired,
+  tags: PropTypes.string.isRequired,
+};
+
 export default ImageGalleryItem;
