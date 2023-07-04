0840d996ff765f48e0818a7f0fa96bede03ff666
Ihor Poliakov <piv2003@gmail.com>
2023-07-04 23:31:30 +0200

    add App

---
+++ src/components/App/App.js
@@ -0,0 +1,7 @@
+import React, { Component } from 'react';
+import ImageGallery from 'components/ImageGallery';
+import Searchbar from 'components/Searchbar';
+import Modal from 'components/Modal';
+import { Wrapper } from './App.styled';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
