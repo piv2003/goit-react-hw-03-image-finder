00b4e73c1c009a2151e48ca70dfdf00a972177b5
Ihor Poliakov <piv2003@gmail.com>
2023-07-04 23:24:44 +0200

    Update ImageGallery.js

--- src/components/ImageGallery/ImageGallery.js
+++ src/components/ImageGallery/ImageGallery.js
@@ -14,6 +14,126 @@ const Status = {
   REJECTED: 'rejected',
 };
 
+class ImageGallery extends Component {
+  state = {
+    status: Status.IDLE,
+    error: null,
+    page: 1,
+    images: [],
+    totalHits: null,
+  };
+
+  async componentDidUpdate(prevProps, _) {
+    try {
+      const prevImgName = prevProps.searchQuery;
+      const nextImgName = this.props.searchQuery;
+
+      if (prevImgName !== nextImgName) {
+        this.setState({
+          images: [],
+          page: 1,
+          status: Status.PENDING,
+        });
+
+        const images = await fetchImagesByName(1, nextImgName);
+
+        this.setState({
+          page: this.state.page + 1,
+          status: Status.RESOLVED,
+          images: images.hits,
+          totalHits: images.totalHits,
+        });
+        if (images.totalHits > 0 && images.totalHits <= 10) {
+          toast.warning("Sorry, there's nothing more to show");
+        }
+        if (images.totalHits > 0) {
+          toast.success(`Success! Found ${images.totalHits} images`);
+        }
+        if (images.totalHits === 0) {
+          toast.error(`Oops! Nothing found. Enter another request`);
+        }
+      }
+    } catch (error) {
+      this.setState({
+        error,
+        status: Status.REJECTED,
+      });
+    }
+  }
+
+  onBtnLoadMore = () => {
+    const imgQuery = this.props.searchQuery;
+    const nextPage = this.state.page;
+
+    this.setState({
+      status: Status.PENDING,
+    });
+
+    fetchImagesByName(nextPage, imgQuery)
+      .then(nextImages => {
+        this.setState({
+          page: this.state.page + 1,
+          status: Status.RESOLVED,
+          images: [...this.state.images, ...nextImages.hits],
+        });
+
+        if (
+          nextImages.totalHits === this.state.images.length ||
+          nextImages.totalHits <
+            this.state.images.length + nextImages.hits.length
+        ) {
+          toast.error(`Sorry we have nothing more to show you.`);
+        }
+      })
+      .catch(error =>
+        this.setState({
+          error,
+          status: Status.REJECTED,
+        })
+      );
+  };
+
+  onCardClick = e => {
+    if (e.currentTarget !== e.target) {
+      const currentImageUrl = e.target.currentSrc;
+      const imageArr = this.state.images.filter(
+        ({ webformatURL }) => webformatURL === currentImageUrl
+      );
+      const largeImageURL = imageArr[0].largeImageURL;
+      const imageTags = imageArr[0].tags;
+
+      this.props.onCardClick(largeImageURL, imageTags);
+      this.props.onOpenModal();
+    }
+  };
+
+  render() {
+    const { status, images, totalHits } = this.state;
+
+    return (
+      <>
+        <ImageGalleryBox onClick={this.onCardClick}>
+          {images.map(({ id, webformatURL, tags }) => {
+            return (
+              <ImageGalleryItem
+                key={id}
+                smallImageURL={webformatURL}
+                tags={tags}
+              />
+            );
+          })}
+        </ImageGalleryBox>
+        {status === Status.RESOLVED &&
+          images.length !== totalHits &&
+          images.length < totalHits && (
+            <ButtonLoadMore onBtnLoadMore={this.onBtnLoadMore} />
+          )}
+        {status === Status.PENDING && <Loader />}
+      </>
+    );
+  }
+}
+
 ImageGallery.propTypes = {
   onCardClick: PropTypes.func.isRequired,
   searchQuery: PropTypes.string.isRequired,
