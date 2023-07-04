1813ff2ff1ad6ff4e3724c99e985f6faf4fe249f
Ihor Poliakov <piv2003@gmail.com>
2023-07-04 16:13:37 +0200

    Update Modal.js

--- src/components/Modal/Modal.js
+++ src/components/Modal/Modal.js
@@ -2,3 +2,24 @@ import React, { Component } from 'react';
 import { createPortal } from 'react-dom';
 import { ModalBox, Overlay } from './Modal.styled';
 import PropTypes from 'prop-types';
+
+const modalRoot = document.querySelector('#modal-root');
+
+class Modal extends Component {
+  componentDidMount() {
+    window.addEventListener('keydown', this.handleEscDown);
+  }
+
+  componentWillUnmount() {
+    window.removeEventListener('keydown', this.handleEscDown);
+  }
+
+  render() {
+    return createPortal(
+      <Overlay onClick={this.handleBackdropClick}>
+        <ModalBox>{this.props.children}</ModalBox>
+      </Overlay>,
+      modalRoot
+    );
+  }
+}
