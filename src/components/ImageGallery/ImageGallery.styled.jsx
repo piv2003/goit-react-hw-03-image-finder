ee837735fc01380d8dc2cdf2d89b1a9762778f8f
Ihor Poliakov <piv2003@gmail.com>
2023-07-04 23:23:12 +0200

    Update ImageGallery.styled.jsx

---
+++ src/components/ImageGallery/ImageGallery.styled.jsx
@@ -0,0 +1,11 @@
+import styled from '@emotion/styled';
+
+export const ImageGalleryBox = styled.ul`
+  display: grid;
+  max-width: calc(100vw - 48px);
+  grid-template-columns: repeat(auto-fill, minmax(320px, 1fr));
+  grid-gap: 16px;
+  margin: 0 auto;
+  padding: 0;
+  list-style: none;
+`;
