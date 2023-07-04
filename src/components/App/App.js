b7084744fb7cc46a661a212161455de0e2f5d107
Ihor Poliakov <piv2003@gmail.com>
2023-07-04 23:33:29 +0200

    Update App.js

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,9 +1,59 @@
 import React, { Component } from 'react';
 import ImageGallery from 'components/ImageGallery';
-import Searchbar from 'components/Searchbar';
+import Searchbar from 'components/SearchBar';
 import Modal from 'components/Modal';
 import { Wrapper } from './App.styled';
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
+
+class App extends Component {
+  state = {
+    searchQuery: '',
+    imgUrl: '',
+    tags: '',
+    showModal: false,
+    buttonDiasbled: true,
+  };
+
+  handleSubmit = searchQuery => {
+    this.setState({
+      searchQuery,
+    });
+  };
+
+  toggleModal = () => {
+    this.setState(({ showModal }) => ({
+      showModal: !showModal,
+    }));
+  };
+
+  onCardClick = (largeImageUrl, imageTags) => {
+    this.setState({
+      imgUrl: largeImageUrl,
+      tags: imageTags,
+    });
+  };
+
+  render() {
+    const { searchQuery, imgUrl, tags, showModal } = this.state;
+
+    return (
+      <Wrapper>
+        <Searchbar onSubmit={this.handleSubmit} searchQuery={searchQuery} />
+        <ImageGallery
+          onCardClick={this.onCardClick}
+          searchQuery={searchQuery}
+          onOpenModal={this.toggleModal}
+        />
+        {showModal && (
+          <Modal onCloseModal={this.toggleModal}>
+            {<img src={imgUrl} alt={tags} />}
+          </Modal>
+        )}
+        <ToastContainer autoClose={3000} theme="dark" />
+      </Wrapper>
+    );
+  }
+}
 
 export default App;
