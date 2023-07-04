2cbf24abdcb557edbd3c0eba72111bb194843567
Ihor Poliakov <piv2003@gmail.com>
2023-07-04 23:38:06 +0200

    adding styled.jsx

---
+++ src/components/App/App.styled.jsx
@@ -0,0 +1,13 @@
+import styled from '@emotion/styled';
+
+export const Wrapper = styled.div`
+  display: grid;
+  grid-template-columns: 1fr;
+  grid-gap: 16px;
+  padding-bottom: 24px;
+
+  & svg {
+    margin-left: auto;
+    margin-right: auto;
+  }
+`;
