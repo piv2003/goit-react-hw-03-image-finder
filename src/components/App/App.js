0b78ecaeb8da69752a25abc864d65a1a8c0c2b0e
MoonDragon <63874017+piv2003@users.noreply.github.com>
2023-07-06 11:43:32 +0200

    Update App.js

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import ImageGallery from 'components/ImageGallery';
-import Searchbar from 'components/SearchBar';
+import Searchbar from 'components/Searchbar';
 import Modal from 'components/Modal';
 import { Wrapper } from './App.styled';
 import { ToastContainer } from 'react-toastify';
